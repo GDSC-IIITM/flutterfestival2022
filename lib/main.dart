ec327a5f7e890604929baa8ada4ebe1eb267f68e
kunaljain0212 <jainkunal209@gmail.com>
2022-03-12 20:54:06 +0530

    flutter festival: create landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,21 +9,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('DSC App'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Flutter Festival',
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.camera_alt),
-          onPressed: () {},
-        ),
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),
       ),
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/landing.png",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'TaskIt',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Color(0xffd76542),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Not your traditional TODO App',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                primary: const Color(0xffd76542),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
