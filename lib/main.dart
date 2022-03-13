a95486dfbb5ecf106a301f71eac2e3b99b4c50a9
Harsh Sharma <bcs_2019023@iiitm.ac.in>
2022-03-13 19:01:54 +0530

    add main page and to-do functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_festival/screens/landing.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -10,54 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),
-      ),
-      home: Scaffold(
-          body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/landing.png",
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'TaskIt',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Color(0xffd76542),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Not your traditional TODO App',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Get Started'),
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                primary: const Color(0xffd76542),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-              ),
-            ),
-          ],
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),
+          colorScheme: const ColorScheme.light(primary: Color(0xffd76542))),
+      home: const Scaffold(
+        body: Center(
+          child: Landing(),
         ),
-      )),
+      ),
     );
   }
 }
